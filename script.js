397512610d1c14cb901f20bc9c4c52b06aa82c40
cybertron1408 <113407974+cybertron1408@users.noreply.github.com>
2025-05-26 15:23:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const inputs = document.querySelectorAll('.controls input');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+   function handleUpdate() {
+  if (this.name === 'volume') {
+    video.volume = this.value;
+  } else if (this.name === 'playbackRate') {
+    video.playbackRate = this.value;
+  } else {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  }
+}
+
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
